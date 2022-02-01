f39b43ee815cf4ec44c8cc5d392c7ab62ed39a69
Nishanth80 <90023265+Nishanth80@users.noreply.github.com>
2022-02-01 10:19:31 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,18 +1,18 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.84.170.22:9000/',
+  serverUrl: 'http://44.201.233.141:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '5c2e067fbf0f29be84519fafc2be1dd94eab6c24',
+	    'sonar.projectName': 'Nodejs',
+	    'sonar.projectKey':'NodeJs',
+	    'sonar.login': 'f977ecac678297e249970bd51df096fba0c1e717',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
-            'sonar.sources': '.',
+            'sonar.sources': '/var/lib/jenkins/workspace/Nodejs/',
 	  //'sonar.tests': 'specs',
           //'sonar.inclusions' : 'src/**'
        },
