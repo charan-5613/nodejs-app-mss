1f2e1f002c6aa4a3df1728dd4f5933d67cdc4cba
charan-5613 <108232565+charan-5613@users.noreply.github.com>
2022-09-08 23:15:11 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -8,7 +8,7 @@ sonarqubeScanner({
 	    'sonar.projectKey':'sonar-demo',
 	    //'sonar.login': 'sqa_2bb8b05844bdacda587c4c379f57c0acc66c71e4',
 	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.password': 'sqa_2bb8b05844bdacda587c4c379f57c0acc66c71e4',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
